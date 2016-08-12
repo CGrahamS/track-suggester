fceda7bd86d0120623379aed5de91bcfa1c176c8
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-08-12 13:09:41 -0700

    add commented out on change function to js, change radio value system in html and js, add var for print outs

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ var result = function(music, vacation, beer, weapon, taco) {
   return music + vacation + beer + weapon + taco;
 };
 
+
+
+
 //front end
 $(function() {
   $("#blanks form").submit(function(event) {
@@ -13,13 +16,25 @@ $(function() {
     var weapon = parseInt($("input:radio[name=weapon]:checked").val());
     var taco = parseInt($("input:radio[name=taco]:checked").val());
     var track = result(music, vacation, beer, weapon, taco);
+    var phpTrack = "should pursue the PHP/Drupal track!";
+    var cssTrack = "should pursue the CSS/Design track!";
+    var javaTrack = "should pursue the Java/Android track!";
+    var tieTrack = "can't decide between the PHP/Drupal or the Java/Android tracks. Try this question to break the tie!"
 
-    if (track >= 40) {
-      $("#track").empty().append("PHP/Drupal track!");
-    } else if (track >= 17) {
-      $("#track").empty().append("CSS/Design track!");
+    $('#my-form input').on('change', function() {
+      alert($('input[name=music]:checked', '#my-form').val());
+    });
+
+
+    console.log(track);
+    if (track >= 13) {
+      $("#track").empty().append(phpTrack);
+    // } else if (track === ) {
+    //   $("#track").empty().append(tieTrack);
+    } else if (track >= 8) {
+        $("#track").empty().append(cssTrack);
     } else {
-      $("#track").empty().append("Java/Android track!");
+      $("#track").empty().append(javaTrack);
     }
 
     $(".name").empty().append(name);
