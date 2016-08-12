036131d680d0687dfe3cf5b4165551c8d53245b8
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-08-12 15:58:07 -0700

    add if else statements for categories implementing toTrack functions in order edge track to appropriate track

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,74 @@
-//back end
-var result = function(music, vacation, beer, weapon, taco) {
-  return music + vacation + beer + weapon + taco;
-};
-var toCss = function(category) {
-   return category +1;
+
+var toJava = function(track) {
+   return track + 0;
  };
-var toPhp = function(category) {
-  return category +10;
+var toCss = function(track) {
+  return track + 1;
+};
+var toPhp = function(track) {
+  return track + 10;
 };
 
-
-//front end
 $(function() {
   $("#blanks form").submit(function(event) {
     var name = $("input#name").val();
-    var music = parseInt($("input:radio[name=music]:checked").val());
-    var vacation = parseInt($("input:radio[name=vacation]:checked").val());
-    var beer = parseInt($("input:radio[name=beer]:checked").val());
-    var weapon = parseInt($("input:radio[name=weapon]:checked").val());
-    var taco = parseInt($("input:radio[name=taco]:checked").val());
-    var track = result(music, vacation, beer, weapon, taco);
+    var music = $("input:radio[name=music]:checked").val();
+    var vacation = $("input:radio[name=vacation]:checked").val();
+    var beer = $("input:radio[name=beer]:checked").val();
+    var weapon = $("input:radio[name=weapon]:checked").val();
+    var taco = $("input:radio[name=taco]:checked").val();
     var phpTrack = "should pursue the PHP/Drupal track!";
     var cssTrack = "should pursue the CSS/Design track!";
     var javaTrack = "should pursue the Java/Android track!";
-    var tieTrack = "can't decide between the PHP/Drupal or the Java/Android tracks. Try this question to break the tie!"
+    var result = 
+
+    if (music === "a") {
+      result = toJava(result);
+    } else if (music === "b") {
+      result = toCss(result);
+    } else if (music === "c"){
+      result = toPhp(result);
+    }
+
+    if (vacation === "a") {
+      result = toJava(result);
+    } else if (vacation === "b") {
+      result = toCss(result);
+    } else if (vacation === "c"){
+      result = toPhp(result);
+    }
+
+    if (beer === "a") {
+      result = toJava(result);
+    } else if (beer === "b") {
+      result = toCss(result);
+    } else if (beer === "c"){
+      result = toPhp(result);
+    }
+
+    if (weapon === "a") {
+      result = toJava(result);
+    } else if (weapon === "b") {
+      result = toCss(result);
+    } else if (weapon === "c"){
+      result = toPhp(result);
+    }
+
+    if (taco === "a") {
+      result = toJava(result);
+    } else if (taco === "b") {
+      result = toCss(result);
+    } else if (taco === "c") {
+      result = toPhp(result);
+    }
+
+    console.log(result);
 
     if (name) {
-      if (track) {
-        if (track >= 13) {
+      if (result) {
+        if (result >= 30) {
           $("#track").empty().append(phpTrack);
-        } else if (track === 11) {
-          $("#track").empty().append(tieTrack);
-        } else if (track >= 8) {
+        } else if (result > 0 && result <= 29) {
             $("#track").empty().append(cssTrack);
         } else {
           $("#track").empty().append(javaTrack);
