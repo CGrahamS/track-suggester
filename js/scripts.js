c94efdc2cfd94d9127e3fde176ad6b4d44128e90
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-08-11 21:57:55 -0700

    add section comments to html, add intial back end and front end to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//back end
+var result = function(music, vaction, beer, villain, hero) {
+  return music + vacation + beer + villain + hero;
+}
+
+//front end
+$(function() {
+  $("#blanks form").submit(function(event) {
+
+    event.preventDefault();
+  });
+});
