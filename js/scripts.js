50602529c4dd8b5c41627bfc545f04ac7c6f179e
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-08-12 08:08:20 -0700

    add variables to front end of JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 //back end
-var result = function(music, vaction, beer, villain, hero) {
+var result = function(music, vaction, beer, weapon, taco) {
   return music + vacation + beer + weapon + taco;
 }
 
 //front end
 $(function() {
   $("#blanks form").submit(function(event) {
+    var name = $("input#name").val();
+    var music = parseInt($("input:radio[name=music]:checked").val());
 
     event.preventDefault();
   });
