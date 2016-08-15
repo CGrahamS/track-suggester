c73fe11223c1f90c38752325a63bc1680488332b
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-08-14 20:12:18 -0700

    remove old java script and add in new java script that uses strings as values and a point system to deteremine the right track for the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,48 @@
-var result = function(music, vacation, beer, weapon, taco) {
-  return music + vacation + beer + weapon + taco;
-};
+var javaTrack = 0;
+var cssTrack = 0;
+var phpTrack = 0;
+var whichTrack = function(answer) {
+  if (answer === "java") {
+    javaTrack += +1;
+  } else if (answer === "css") {
+    cssTrack += +1;
+  } else if (answer === "php") {
+    phpTrack += +1;
+  }
+}
+
 
 $(function() {
   $("#blanks form").submit(function(event) {
     var name = $("input#name").val();
-    var music = parseInt($("input:radio[name=music]:checked").val());
-    var vacation = parseInt($("input:radio[name=vacation]:checked").val());
-    var beer = parseInt($("input:radio[name=beer]:checked").val());
-    var weapon = parseInt($("input:radio[name=weapon]:checked").val());
-    var taco = parseInt($("input:radio[name=taco]:checked").val());
-    var track = result(music, vacation, beer, weapon, taco);
-    var phpTrack = "should pursue the PHP/Drupal track!";
-    var cssTrack = "should pursue the CSS/Design track!";
-    var javaTrack = "should pursue the Java/Android track!";
-    var tieTrack = "can't decide between the PHP/Drupal or the Java/Android tracks!";
+    var music = $("input:radio[name=music]:checked").val();
+    var vacation = $("input:radio[name=vacation]:checked").val();
+    var beer = $("input:radio[name=beer]:checked").val();
+    var weapon = $("input:radio[name=weapon]:checked").val();
+    var taco = $("input:radio[name=taco]:checked").val();
 
+    whichTrack(music);
+    whichTrack(vacation);
+    whichTrack(beer);
+    whichTrack(weapon);
+    whichTrack(taco);
     if (name) {
       if (track) {
-        if (track >= 13) {
-          $("#track").empty().append(phpTrack);
-        } else if (track === 10) {
-          $("#track").empty().append(tieTrack);
-        } else if (track >= 8) {
-            $("#track").empty().append(cssTrack);
-        } else {
-          $("#track").empty().append(javaTrack);
+        if (javaTrack > cssTrack && javaTrack > phpTrack) {
+          $("#track").empty().append("java");
+        } else if (javaTrack === cssTrack || javaTrack === phpTrack || cssTrack === phpTrack) {
+          $("#track").empty().append("tie");
+        } else if (cssTrack > javaTrack && cssTrack > phpTrack) {
+            $("#track").empty().append("css");
+        } else if (phpTrack > javaTrack && phpTrack > cssTrack) {
+          $("#track").empty().append("php");
         }
 
         $(".name").empty().append(name);
         $("#output").fadeIn();
+        javaTrack = 0;
+        cssTrack = 0;
+        phpTrack = 0;
       } else {
         alert("Please pick some options!");
       }
